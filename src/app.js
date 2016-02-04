bde1abc8086d80f01ee330e45aaa35cf3aee201e
Alejandro Nanez <alejonanez@gmail.com>
2016-02-03 19:44:15 -0500

    Basic file structure.

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
+require('bootstrap/dist/css/bootstrap.min.css');
+
+const API_KEY = require('./youtube-api.json').API_KEY;
 
 render(<h1>Hi - React Starter</h1>, document.getElementById('root'));
