76e6d34b873902552e020cb0644c06f84b83b185
Alejandro Nanez <alejonanez@gmail.com>
2016-02-08 18:46:56 -0500

    Small refactor of <App />

--- src/app.js
+++ src/app.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { render } from 'react-dom';
+import YTSearch from 'youtube-api-search';
 require('bootstrap/dist/css/bootstrap.min.css');
 
 import SearchBar from './components/search-bar';
 const API_KEY = require('./youtube-api.json').API_KEY;
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
-};
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
+}
 
 
 render(<App />, document.getElementById('root'));
